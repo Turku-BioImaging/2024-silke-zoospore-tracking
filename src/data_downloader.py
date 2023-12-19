4aacba370aebafa792b54599a7e6625150407bb2
Junel Solis <junel.solis@abo.fi>
2023-12-19 20:54:17 +0200

    make conversion use multiprocessing

--- src/data_downloader.py
+++ src/data_downloader.py
@@ -4,20 +4,26 @@ Author: Junel Solis, Turku BioImaging, Turku, Finland
 
 """
 import os
-import swiftclient
-from dotenv import load_dotenv
-import numpy as np
-import os
 import random
-from tqdm import tqdm
-from aicsimageio import AICSImage
 from glob import glob
+
+import numpy as np
+import swiftclient
+from aicsimageio import AICSImage
+from dotenv import load_dotenv
 from skimage import io
+from tqdm import tqdm
+import multiprocessing as mp
 
 random.seed(858)
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data", "nd2")
-TIFF_DIR = os.path.join(os.path.dirname(__file__), "data", "tiff")
+# TIFF_DIR = os.path.join(os.path.dirname(__file__), "data", "tiff")
+TIFF_DIR = os.path.join("/", "mnt", "f", "silke-convert-temp")
+
+EXCLUSION_LIST = [
+    "FastTimeLaps_exp_02_StauChy1_230120_rep3/FastTimeLaps_exp_test1_1_from0.nd2"
+]
 
 
 # Configure CSC Allas connection
@@ -39,10 +45,11 @@ conn = swiftclient.Connection(
 
 resp_headers, containers = conn.get_account()
 
-# Download random files from the container
 os.makedirs(DATA_DIR, exist_ok=True)
 
-file_paths = random.choices(conn.get_container("zoospore-time-lapse-data")[1], k=10)
+# file_paths = random.choices(conn.get_container("zoospore-time-lapse-data")[1], k=10)
+file_paths = conn.get_container("zoospore-time-lapse-data")[1]
+
 for fpath in tqdm(file_paths):
     if not os.path.exists(os.path.join(DATA_DIR, fpath["name"])):
         obj = conn.get_object("zoospore-time-lapse-data", fpath["name"])[1]
@@ -50,21 +57,41 @@ for fpath in tqdm(file_paths):
         dir_path = fpath["name"].split("/")[0]
         os.makedirs(os.path.join(DATA_DIR, dir_path), exist_ok=True)
 
+        # check that file is not write-protected
+        if not os.access(os.path.join(DATA_DIR, fpath["name"]), os.W_OK):
+            continue
+
         with open(os.path.join(DATA_DIR, fpath["name"]), "wb") as f:
             f.write(obj)
 
 
 # Convert ND2 files to TIFF
-nd2_paths = glob(os.path.join(DATA_DIR, "**", "*.nd2"), recursive=True)
-for p in tqdm(nd2_paths):
-    img = AICSImage(p)
-    img = np.squeeze(img.data, axis=(1, 2))
-    img = np.moveaxis(img, -1, 1)
-
+def convert_nd2_to_tiff(p):
     dir_path = os.path.dirname(p).replace(DATA_DIR, TIFF_DIR).split("/")[-1]
     fname = os.path.basename(p).split(".")[0] + ".tif"
+
+    if os.path.isfile(os.path.join(TIFF_DIR, dir_path, fname)):
+        return
+
+    # do not process files in exclusion list
+    for exclusion in EXCLUSION_LIST:
+        if exclusion in p:
+            return
 
     if not os.path.exists(os.path.join(TIFF_DIR, dir_path)):
         os.makedirs(os.path.join(TIFF_DIR, dir_path))
 
-    io.imsave(os.path.join(TIFF_DIR, dir_path, fname), img, check_contrast=False, imagej=True)
+    img = AICSImage(p)
+    img = np.squeeze(img.data, axis=(1, 2))
+    img = np.moveaxis(img, -1, 1)
+    io.imsave(
+        os.path.join(TIFF_DIR, dir_path, fname), img, check_contrast=False, imagej=True
+    )
+
+
+nd2_paths = glob(os.path.join(DATA_DIR, "**", "*.nd2"), recursive=True)
+with mp.Pool() as pool:
+    max_ = len(nd2_paths)
+    with tqdm(total=max_) as pbar:
+        for i, _ in enumerate(pool.imap_unordered(convert_nd2_to_tiff, nd2_paths)):
+            pbar.update()
