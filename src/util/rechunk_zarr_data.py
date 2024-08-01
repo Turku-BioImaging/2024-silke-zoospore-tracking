1e07d8b5188e29eef95ad4aa70f6e0a25bf02838
Junel Solis <junel.solis@abo.fi>
2024-08-01 22:51:13 +0300

    rechunking utility script

--- src/util/rechunk_zarr_data.py
+++ src/util/rechunk_zarr_data.py
@@ -3,6 +3,7 @@ Rechunk the zarr datasets using dask arrays. This will allow for larger chunks a
 """
 
 import os
+import shutil
 import zarr
 import dask.array as da
 from tqdm import tqdm
@@ -30,16 +31,12 @@ def _process_exp(replicate: str, exp: str) -> None:
             url=ZARR_PATH, component=f"{replicate}/{exp}/detection_da", overwrite=True
         )
 
-        # del root[f"{replicate}/{exp}/detection"]
-
     if "linking" in root[replicate][exp]:
         linking_da = da.from_zarr(root[replicate][exp]["linking"])  # type: ignore
         linking_da = linking_da.rechunk()
         linking_da.to_zarr(
             url=ZARR_PATH, component=f"{replicate}/{exp}/linking_da", overwrite=True
         )
-
-        # del root[f"{replicate}/{exp}/linking"]
 
     if "raw_data_da" in root[replicate][exp]:
         del root[f"{replicate}/{exp}/raw_data_da"]
@@ -48,13 +45,15 @@ def _process_exp(replicate: str, exp: str) -> None:
 def _cleanup_dirs(replicate: str, exp: str) -> None:
     # remove old detection and linking directories
     detection_path = os.path.join(ZARR_PATH, replicate, exp, "detection")
+    new_detection_path = os.path.join(ZARR_PATH, replicate, exp, "detection_da")
     linking_path = os.path.join(ZARR_PATH, replicate, exp, "linking")
+    new_linking_path = os.path.join(ZARR_PATH, replicate, exp, "linking_da")
 
-    if os.path.isdir(detection_path):
-        os.rmdir(detection_path)
+    if os.path.isdir(detection_path) and os.path.isdir(new_detection_path):
+        shutil.rmtree(detection_path)
 
-    if os.path.isdir(linking_path):
-        os.rmdir(linking_path)
+    if os.path.isdir(linking_path) and os.path.isdir(new_linking_path):
+        shutil.rmtree(linking_path)
 
     # rename the new detection and linking directories
     detection_path = os.path.join(ZARR_PATH, replicate, exp, "detection_da")
@@ -64,31 +63,10 @@ def _cleanup_dirs(replicate: str, exp: str) -> None:
     os.rename(linking_path, os.path.join(ZARR_PATH, replicate, exp, "linking"))
 
 
-# for replicate, exp in exp_data:
+# Parallel(n_jobs=1, verbose=10)(
+#     delayed(_process_exp)(replicate, exp) for replicate, exp in exp_data
+# )
 
-#     if "detection" in root[replicate][exp]:
-#         detection_da = da.from_zarr(root[replicate][exp]["detection"])  # type: ignore
-#         detection_da = detection_da.rechunk()
-#         detection_da.to_zarr(
-#             url=ZARR_PATH, component=f"{replicate}/{exp}/detection_da", overwrite=True
-#         )
-
-#         # del root[f"{replicate}/{exp}/detection"]
-
-#     if "linking" in root[replicate][exp]:
-#         linking_da = da.from_zarr(root[replicate][exp]["linking"])  # type: ignore
-#         linking_da = linking_da.rechunk()
-#         linking_da.to_zarr(
-#             url=ZARR_PATH, component=f"{replicate}/{exp}/linking_da", overwrite=True
-#         )
-
-#         # del root[f"{replicate}/{exp}/linking"]
-
-#     if "raw_data_da" in root[replicate][exp]:
-#         del root[f"{replicate}/{exp}/raw_data_da"]
-
-Parallel(n_jobs=4, verbose=10)(
-    delayed(_process_exp)(replicate, exp) for replicate, exp in exp_data
+Parallel(n_jobs=-1)(
+    delayed(_cleanup_dirs)(replicate, exp) for replicate, exp in tqdm(exp_data)
 )
-
-# Parallel(n_jobs=-1)(delayed(_cleanup_dirs)(replicate, exp) for replicate, exp in tqdm(exp_data))
